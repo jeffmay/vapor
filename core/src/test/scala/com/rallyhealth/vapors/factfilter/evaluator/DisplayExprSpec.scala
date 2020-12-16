d644513d3c251b6ba5d9f05ec9d2107bccead1b8
Jeff May <jeff.n.may@gmail.com>
2020-12-15 19:20:24 -0800

    WIP on better serialization

---
+++ core/src/test/scala/com/rallyhealth/vapors/factfilter/evaluator/DisplayExprSpec.scala
@@ -0,0 +1,21 @@
+package com.rallyhealth.vapors.factfilter.evaluator
+
+import com.rallyhealth.vapors.factfilter.Example.FactTypes
+import com.rallyhealth.vapors.factfilter.dsl.CaptureP.unit._
+import org.scalatest.wordspec.AnyWordSpec
+import com.rallyhealth.vapors.factfilter.dsl.ExprDsl._
+
+class DisplayExprSpec extends AnyWordSpec {
+
+  "DisplayExpr" should {
+
+    "display returnInput" in {
+      val expr = withFactsOfType(FactTypes.Age).returnInput
+      assertResult {
+        """"""
+      } {
+        DisplayExpr.serialize(expr)
+      }
+    }
+  }
+}
